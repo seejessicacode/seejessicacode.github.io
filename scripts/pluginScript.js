ad26e59fc8ab12b53add7031112490c39895f8ce
Jessica Campbell <jesslyn.campbell@gmail.com>
2014-09-13 11:53:51 -0500

    demo tag plugin script

---
+++ scripts/pluginScript.js
@@ -0,0 +1,3 @@
+hexo.extend.tag.register('hexo-test-plugin-script-copy', function(args, content) {
+  return "hexo-test-plugin-script-copy";
+}, true);
---
+++ source/_posts/post2.md
@@ -0,0 +1,6 @@
+title: post2
+date: 2014-09-13 11:51:16
+tags:
+---
+
+{% hexo-test-plugin-script-copy  %}
